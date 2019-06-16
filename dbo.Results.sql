a79d5406fd876d1d079bbbab978f54007c594d64
zeytunyan <47988040+zeytunyan@users.noreply.github.com>
2019-06-16 23:13:23 +0300

    Add files via upload

---
+++ dbo.Results.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Results] (
+    [Id]    INT             IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (255)  NOT NULL,
+    [Coeff] DECIMAL (18, 5) NOT NULL,
+    [Concl] NVARCHAR (255)  NOT NULL,
+    [Time]  DATETIME        DEFAULT (getdate()) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
