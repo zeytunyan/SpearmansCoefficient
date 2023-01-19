a6083b16b67f4d192b0710c14db7b5b534a37a5c
zeytunyan <georgiy.zeytunyan@gmial.com>
2023-01-19 17:47:40 +0300

    Fix

--- dbo.Variables.sql
+++ dbo.Variables.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿CREATE TABLE [dbo].[Variables] (
     [Id]  INT IDENTITY (1, 1) NOT NULL,
     [RID] INT NOT NULL,
@@ -8,14 +7,3 @@
     FOREIGN KEY ([RID]) REFERENCES [dbo].[Results] ([Id])
 );
 
-=======
-﻿CREATE TABLE [dbo].[Variables] (
-    [Id]  INT IDENTITY (1, 1) NOT NULL,
-    [RID] INT NOT NULL,
-    [X]   INT NOT NULL,
-    [Y]   INT NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    FOREIGN KEY ([RID]) REFERENCES [dbo].[Results] ([Id])
-);
-
->>>>>>> 2ff17273403c36b340e770197aef05c8910e6989
