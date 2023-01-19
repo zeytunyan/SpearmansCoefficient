c0a19a48df86027d787ebfb2695ca67d63ffdbe7
zeytunyan <georgiy.zeytunyan@gmial.com>
2023-01-19 17:42:28 +0300

    Some changes

---
+++ dbo.Variables.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Variables] (
+    [Id]  INT IDENTITY (1, 1) NOT NULL,
+    [RID] INT NOT NULL,
+    [X]   INT NOT NULL,
+    [Y]   INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([RID]) REFERENCES [dbo].[Results] ([Id])
+);
+
